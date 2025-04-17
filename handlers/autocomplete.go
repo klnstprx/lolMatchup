4ebda5672e603938d154d837da9802cce6a5c1f8
klnstprx <ignacy.borzestowski@gmail.com>
2025-04-17 22:01:00 +0900

    refactoring autocomplete + tests

--- handlers/autocomplete.go
+++ handlers/autocomplete.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"net/http"
-	"sort"
 	"strings"
 
 	"github.com/charmbracelet/log"
@@ -33,41 +32,11 @@ func NewAutocompleteHandler(cfg *config.AppConfig, apiClient *client.Client) *Au
 // AutocompleteGET handles /autocomplete requests for champion names.
 func (h *AutocompleteHandler) AutocompleteGET(c *gin.Context) {
 	userQuery := strings.TrimSpace(c.Query("champion"))
-	if userQuery == "" {
-		empty := components.ChampionAutocomplete([]string{})
-		c.Render(http.StatusOK, renderer.New(c.Request.Context(), http.StatusOK, empty))
-		return
+	// Get up to 10 best fuzzy/autocomplete matches
+	var suggestions []string
+	if userQuery != "" {
+		suggestions = h.Cache.Autocomplete(userQuery, 10)
 	}
-
-	queryLower := strings.ToLower(userQuery)
-	championMap := h.Cache.GetChampionMap()
-
-	// Gather all champion names in a slice for alphabetical sorting
-	allNames := make([]string, 0, len(championMap))
-	for name := range championMap {
-		allNames = append(allNames, name)
-	}
-
-	sort.Strings(allNames) // alphabetical order
-
-	var prefixMatches, substringMatches []string
-
-	for _, name := range allNames {
-		nameLower := strings.ToLower(name)
-		if strings.HasPrefix(nameLower, queryLower) {
-			prefixMatches = append(prefixMatches, name)
-		} else if strings.Contains(nameLower, queryLower) {
-			substringMatches = append(substringMatches, name)
-		}
-	}
-
-	// Combine prefix matches (first) and substring matches (second)
-	results := append(prefixMatches, substringMatches...)
-	// Trim to a maximum of 10
-	if len(results) > 10 {
-		results = results[:10]
-	}
-
-	autocomplete := components.ChampionAutocomplete(results)
-	c.Render(http.StatusOK, renderer.New(c.Request.Context(), http.StatusOK, autocomplete))
+	comp := components.ChampionAutocomplete(suggestions)
+	c.Render(http.StatusOK, renderer.New(c.Request.Context(), http.StatusOK, comp))
 }
